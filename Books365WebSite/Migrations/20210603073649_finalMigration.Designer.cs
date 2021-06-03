32e13c13ebc5153c5305ec1f2216a9c5db347f27
volodymyr.shabat <volodymyr.shabat@intenselab.com>
2021-06-03 10:44:08 +0300

    Added migration

--- Books365WebSite/Migrations/20210603073649_finalMigration.Designer.cs
+++ Books365WebSite/Migrations/20210603073649_finalMigration.Designer.cs
@@ -4,14 +4,16 @@ using Books365WebSite.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Books365WebSite.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210603073649_finalMigration")]
+    partial class finalMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
